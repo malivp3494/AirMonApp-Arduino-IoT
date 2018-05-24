030d7c8892050bc330d43dc15f9a89794ebeece7
Vaibhav Mali <malivp3494@gmail.com>
2018-05-24 09:54:19 +0530

    Its working

---
+++ F1.ino
@@ -0,0 +1,88 @@
+#include<ESP8266WiFi.h>
+#include<FirebaseArduino.h>
+#include "UbidotsMicroESP8266.h"
+
+#define TOKEN "BBFF-FoZI7yhMUQVjqv8ApwfqMgBUvAyBuO"
+Ubidots client(TOKEN);
+#define FIREBASE_HOST "airmonapp.firebaseio.com"
+#define FIREBASE_AUTH "hQ5MvC54UNK3UVNr940KqBzhFAwWGV6Tp7seW5Bu"
+#define WIFI_SSID "JioFi4_06601F"
+#define WIFI_PASSWORD "ze0mmp5ic9"
+
+
+void setup() 
+{
+  pinMode(A0,INPUT);
+  pinMode(D7,INPUT);
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+  client.wifiConnection(WIFI_SSID, WIFI_PASSWORD);
+  
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  Firebase.set("rain/name","rain");
+  Firebase.set("rain/status","NORMAL");
+  Firebase.set("rain/value",1);
+  Firebase.set("LDR/name","LDR");
+  Firebase.set("LDR/status","NORMAL");
+  Firebase.set("LDR/value",0);
+}
+
+void firebasereconnect(){
+  Serial.println("Trying to reconnect");
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop()
+{
+  int rainVal = digitalRead(D7);
+  int LDR=analogRead(A0);
+ 
+  //Firebase.set("rain",rainVal);
+  //Firebase.set("LDR",LDR);
+  
+  if(rainVal==1){
+    Serial.println("Not Raining");
+      Firebase.set("rain/name","rain");
+      Firebase.set("rain/status","NORMAL");
+      Firebase.set("rain/value",rainVal); 
+  }
+  else if(rainVal==0){
+    Serial.println("Raining");
+      Firebase.set("rain/name","rain");
+      Firebase.set("rain/status","DANGER");
+      Firebase.set("rain/value",rainVal);
+  }
+
+  if(LDR>=800 || LDR<100){
+    Firebase.set("LDR/name","LDR");
+    Firebase.set("LDR/status","DANGER");
+    Firebase.set("LDR/value",LDR);
+  }
+  else if(LDR<800 && LDR>100){
+    Firebase.set("LDR/name","LDR");
+    Firebase.set("LDR/status","NORMAL");
+    Firebase.set("LDR/value",LDR);
+  }
+  Serial.print("Light intensity is: ");
+  Serial.println(LDR);
+  delay(1000);
+
+  {
+    client.add("Rain",rainVal);
+    client.add("LDR",LDR);
+    client.sendAll(true);
+    delay(1000);  
+  }
+ 
+}
---
+++ F2.ino
@@ -0,0 +1,117 @@
+#include "DHT.h"
+#define DHTTYPE DHT11 
+#include<ESP8266WiFi.h>
+#include<FirebaseArduino.h>
+#include "UbidotsMicroESP8266.h"
+#define UBTOKEN "BBFF-FoZI7yhMUQVjqv8ApwfqMgBUvAyBuO"
+Ubidots client(UBTOKEN);
+#define FIREBASE_HOST "airmonapp.firebaseio.com"
+#define FIREBASE_AUTH "hQ5MvC54UNK3UVNr940KqBzhFAwWGV6Tp7seW5Bu"
+#define WIFI_SSID "JioFi4_06601F"
+#define WIFI_PASSWORD "ze0mmp5ic9"
+ 
+float hum;
+float temp;
+ 
+DHT dht(D7, DHTTYPE);
+
+void setup(){
+    Serial.begin(9600);
+    delay(10);
+    pinMode(A0,INPUT);
+    dht.begin();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.set("humidity/name","humidity");
+  Firebase.set("humidity/status","NORMAL");
+  Firebase.set("humidity/value",0);
+  Firebase.set("temperature/name","temperature");
+  Firebase.set("temperature/status","NORMAL");
+  Firebase.set("temperature/value",0);
+  Firebase.set("smoke/name","smoke");
+  Firebase.set("smoke/status","NORMAL");
+  Firebase.set("smoke/value",0);
+}
+
+void firebasereconnect(){
+  Serial.println("Trying to reconnect");
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop()
+{
+    if(Firebase.failed()){
+    Serial.print("setting number failed");
+    Serial.println(Firebase.error());
+    firebasereconnect();
+    return;
+  }
+  
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  int smoke=analogRead(A0);
+  delay(1000);
+
+  if(hum<30 || hum>70){
+    Firebase.set("humidity/name","humidity");
+    Firebase.set("humidity/status","DANGER");
+    Firebase.set("humidity/value",hum);
+  }
+  else if(hum>30 && hum<70){
+    Firebase.set("humidity/name","humidity");
+    Firebase.set("humidity/status","NORMAL");
+    Firebase.set("humidity/value",hum);
+  }
+
+  if(temp<10 || temp>40){
+    Firebase.set("temperature/name","temperature");
+    Firebase.set("temperature/status","DANGER");
+    Firebase.set("temperature/value",temp);
+  }
+  else if(temp>10 && temp<40){
+    Firebase.set("temperature/name","temperature");
+    Firebase.set("temperature/status","NORMAL");
+    Firebase.set("temperature/value",temp);
+  }
+
+  if(smoke>250){
+    Firebase.set("smoke/name","smoke");
+    Firebase.set("smoke/status","DANGER");
+    Firebase.set("smoke/value",smoke);
+  }
+  else if(smoke<250){
+    Firebase.set("smoke/name","smoke");
+    Firebase.set("smoke/status","NORMAL");
+    Firebase.set("smoke/value",smoke);
+  }
+  
+  Serial.print(temp);
+  Serial.print("\t");
+  Serial.print(hum);
+  Serial.print("\t");
+  Serial.print(smoke);
+  Serial.println();
+  if (isnan(hum) || isnan(temp)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  {
+    client.add("Hum",hum);
+    client.add("Tmp",temp);
+    client.add("Smoke",smoke);
+    client.sendAll(true);
+    delay(1000);  
+  }
+
+  }
+
